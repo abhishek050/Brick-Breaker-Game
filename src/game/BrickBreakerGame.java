e7d9b29e3dbe14716dbc242fd728eb237e598067
abhishek050 <abhishekupadhyay050@gmail.com>
2021-01-07 01:36:42 +0530

    First commit

---
+++ src/game/BrickBreakerGame.java
@@ -0,0 +1,20 @@
+
+package brick.breaker.game;
+
+import javax.swing.JFrame;
+
+
+public class BrickBreakerGame {
+
+    public static void main(String[] args) {
+       JFrame obj = new JFrame();
+       Gameplay gamePlay = new Gameplay();
+       obj.setBounds(10,10,700,600);
+       obj.setTitle("Breakout Ball");
+       obj.setResizable(false);
+       obj.setVisible(true);
+       obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       obj.add(gamePlay);
+    }
+    
+}
